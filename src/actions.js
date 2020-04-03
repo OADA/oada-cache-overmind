962aa5034c65dc6267cc6f304f44b4c3182a5e12
Cyrus Bowman <cyrusbow@gmail.com>
2020-04-02 23:36:46 -0400

    - Finished port. Need to test.

--- src/actions.js
+++ src/actions.js
@@ -63,16 +63,16 @@ export default function(_namespace) {
         if (request.complete) return
         let _statePath = request.path.replace(/^\//, '').split('/').join('.')
         if (request.watch) {
-          let conn = state.oada[(request.connection_id || props.connection_id)]; //TODO namespace
+          let conn = state[(request.connection_id || props.connection_id)];
           if (!conn) {
             if (conn && conn.watches && conn.watches[request.path]) return
-            request.watch.signals = [actions.oada.handleWatch, ...request.watch.actions]; //TODO namespace
+            request.watch.signals = [actions.handleWatch, ...request.watch.actions];
             request.watch.payload = request.watch.payload || {};
             request.watch.payload.connection_id = request.connection_id || props.connection_id;
             request.watch.payload.path = _statePath;
           }
         }
-        return effects.oada.get({ //TODO namespace
+        return effects.get({
           connection_id: request.connection_id || props.connection_id,
           url: request.url,
           path: request.path,
@@ -82,11 +82,11 @@ export default function(_namespace) {
         }).then((response) => {
           let _responseData = response.data;
           //Build out path one object at a time.
-          var path = `oada.${request.connection_id || props.connection_id}.${_statePath}`; //TODO namespace
+          var path = `${request.connection_id || props.connection_id}.${_statePath}`;
           //Set response
           if (_responseData) _.set(state, path, _responseData);
           if (request.watch) {
-            path = `oada.${request.connection_id || props.connection_id}.watches.${request.path}`; //TODO namespace
+            path = `${request.connection_id || props.connection_id}.watches.${request.path}`;
             _.set(state, path, true);
             store.set(state`${path}`, true)
           }
@@ -101,9 +101,105 @@ export default function(_namespace) {
         return {responses, requests}
       })
     },
+    put(context, props) {
+      const {state, effects, actions} = ns(context);
+      if (!props.requests) throw new Error("Missing requests. Please pass requests in as an array of request objects under the requests key");
+      var requests = props.requests || [];
+      return Promise.map(requests, (request, i) => {
+        if (request.complete) return;
+        return effects.put({
+          url: request.url, //props.domain + ((request.path[0] === '/') ? '':'/') + request.path,
+          path: request.path,
+          data: request.data,
+          type: request.type,
+          headers: request.headers,
+          tree: request.tree || props.tree,
+          connection_id: request.connection_id || props.connection_id,
+        }).then((response) => {
+          var path = `${request.connection_id || props.connection_id}.${request.path.split("/").join(".")}`;
+          var oldState = _.cloneDeep(_.get(state, path));
+          var newState = _.merge(oldState, request.data);
+          _.set(state, path, newState)
+          requests[i].complete = true;
+          return response;
+        });
+      }).then((responses) => {
+        return { responses, requests };
+      });
+    },
+    post(context, props) {
+      const {state, effects, actions} = ns(context);
+      if (!props.requests) throw new Error("Missing requests. Please pass requests in as an array of request objects under the requests key");
+      var requests = props.requests || [];
+      return Promise.map(requests, (request, i) => {
+        if (request.complete) return;
+        return effects.post({
+            url: request.url, //props.domain + ((request.path[0] === '/') ? '':'/') + request.path,
+            path: request.path,
+            data: request.data,
+            type: request.type,
+            headers: request.headers,
+            tree: request.tree || props.tree,
+            connection_id: request.connection_id || props.connection_id,
+          })
+          .then((response) => {
+            var id = response.headers.location; //TODO why is this here?
+            var path = `${request.connection_id || props.connection_id}.${request.path.split("/").join(".")}`;
+            var oldState = _.cloneDeep(_.get(state, path));
+            var newState = _.merge(oldState, request.data);
+            _.set(state, path, newState)
+            requests[i].complete = true;
+            return;
+          });
+      }).then((responses) => {
+        return { responses };
+      });
+    },
+    delete(context, props) {
+      const {state, effects, actions} = ns(context);
+      if (!props.requests) throw new Error("Missing requests. Please pass requests in as an array of request objects under the requests key");
+      var requests = props.requests || [];
+      const connectionId =
+      return Promise.map(requests, (request, i) => {
+        if (request.complete) return;
+        const connectionId = request.connection_id || props.connection_id;
+        let _statePath = request.path.replace(/^\//, "").split("/").join(".");
+        let conn = _.get(state, connectionId);
+        if (request.unwatch && conn && conn.watches) {
+          // Don't send the unwatch request if it isn't being watched already.
+          if (!conn.watches[request.path]) return;
+        }
+        return effects.delete({
+            connection_id: connectionId,
+            url: request.url,
+            path: request.path,
+            headers: request.headers,
+            unwatch: request.unwatch,
+            type: request.type,
+            tree: request.tree || props.tree,
+        })
+        .then((response) => {
+          //Handle watches index and optimistically update
+          if (request.unwatch && conn && conn.watches) {
+            _.unset(state,`${connectionId}.watches.${request.path}`);
+          } else {
+            _.unset(state,`${connectionId}.${_statePath}`);
+          }
+          requests[i].complete = true;
+          return response;
+        });
+      }).then((responses) => {
+        return { responses, requests };
+      });
+    },
     disconnect(context, props) {
       const {state, effects} = ns(context);
-      return effects.oada.disconnect({connection_id: props.connection_id}); //TODO namespace
+      return effects.disconnect({connection_id: props.connection_id});
+    },
+    resetCache(context, props) {
+      return effects.resetCache({
+        connection_id: props.connection_id || domainToConnectionId(props.domain)
+      });
     },
     test(context, props) {
       const {state} = ns(context);
